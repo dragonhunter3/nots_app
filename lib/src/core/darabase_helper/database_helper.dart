ccdae5709f28d47541d1c5e5db927b18d9c6b9e1
dragonhunter3 <khanibloch88@gmail.com>
2025-06-23 00:50:36 +0500

    add code

---
+++ lib/src/core/darabase_helper/database_helper.dart
@@ -0,0 +1,55 @@
+import 'package:noots_app/src/common/model/notes_model.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  static Database? _database;
+  factory DatabaseHelper() => _instance;
+
+  DatabaseHelper._internal();
+  Future<Database> get database async {
+    if (_database != Null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), "my_notes.db");
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+CREATE TABLE NOTES(
+id INTEGER PRIMERY KEY AUTOINCREMENT,
+title Text,
+discription Text,
+color Text,
+dateTime Text
+)
+''');
+  }
+
+  Future<int> insertNote(NotesModel note) async {
+    final db = await database;
+    return await db.insert("notes", note.toMap());
+  }
+
+  Future<List<NotesModel>> getNotes() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query("notes");
+    return List.generate(maps.length, (i) => NotesModel.fromMap(maps[i]));
+  }
+
+  Future<int> updateNote(NotesModel note) async {
+    final db = await database;
+    return await db
+        .update("notes", note.toMap(), where: 'id = ?', whereArgs: [note.id]);
+  }
+
+  Future<int> deleteNote(NotesModel note) async {
+    final db = await database;
+    return await db.delete("notes", where: 'id = ?', whereArgs: [note.id]);
+  }
+}
--- lib/src/featues/home_page/home_page.dart
+++ lib/src/featues/home_page/home_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:noots_app/src/common/constants/global_variables.dart';
+import 'package:noots_app/src/common/model/notes_model.dart';
+import 'package:noots_app/src/core/darabase_helper/database_helper.dart';
+import 'package:noots_app/src/featues/add_edit/add_edit_screen.dart';
+import 'package:noots_app/src/featues/view_notes/view_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +13,108 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+  List<NotesModel> _notes = [];
+  final List<Color> _noteColors = [
+    Colors.amber,
+    Color(0xff50c878),
+    Colors.redAccent,
+    Colors.blueAccent,
+    Colors.indigo,
+    Colors.purpleAccent,
+    Colors.pinkAccent
+  ];
+  @override
+  void initState() {
+    super.initState();
+    _loadNots();
+  }
+
+  Future<void> _loadNots() async {
+    final notes = await _databaseHelper.getNotes();
+    setState(() {
+      _notes = notes;
+    });
+  }
+
+  String _formateDateTime(String dateTime) {
+    final DateTime dt = DateTime.parse(dateTime);
+    final now = DateTime.now();
+    if (dt.year == now.year && dt.month == now.month && dt.day == now.day) {
+      return 'Today ${dt.hour.toString().padLeft(2, '0')}:${dt.minute.toString().padLeft(0, '0')}';
+    }
+    return '${dt.day}/${dt.month}/${dt.year}, ${dt.hour.toString().padLeft(2, '0')}:${dt.minute.toString().padLeft(0, '0')}';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+            "Notes App",
+            style: textTheme(context)
+                .headlineMedium
+                ?.copyWith(fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true),
+      body: GridView.builder(
+        padding: EdgeInsets.all(16),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 16, mainAxisSpacing: 16),
+        itemCount: _notes.length,
+        itemBuilder: (context, index) {
+          final notes = _notes[index];
+          final color = Color(int.parse(notes.color!));
+          return GestureDetector(
+            onTap: () async {
+              // await Navigator.push(context, MaterialPageRoute(builder:(context) => ViewScreen(note:notes),));
+              _loadNots();
+            },
+            child: Card(
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20), color: color),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      notes.title!,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: textTheme(context)
+                          .headlineSmall
+                          ?.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(notes.discription!,
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                        style: textTheme(context).titleMedium),
+                    Spacer(),
+                    Text(
+                      _formateDateTime(notes.dateTime!),
+                      style: textTheme(context).titleSmall?.copyWith(
+                          fontWeight: FontWeight.bold, color: Colors.red),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddEditScreen(),
+              ));
+          _loadNots();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
